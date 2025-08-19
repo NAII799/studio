03935487b2493000cffb2ab93e9d86eea2d87826
NAI <ni11222266@gmail.com>
2025-08-19 13:57:34 +0000

    شوف

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,19 @@ export default function Home() {
   const [passengerToPrint, setPassengerToPrint] = useState<CheckedInPassenger | null>(null);
 
   return (
-    <div className="flex flex-col min-h-screen bg-background no-print">
-      <AirportHeader />
-      <main className="flex-1 flex flex-col items-center justify-center p-4 md:p-10">
-        <CheckInFlow onPrintRequest={setPassengerToPrint} />
-      </main>
-      <AirportFooter />
+    <>
+      <div className="flex flex-col min-h-screen bg-background no-print">
+        <AirportHeader />
+        <main className="flex-1 flex flex-col items-center justify-center p-4 md:p-10">
+          <CheckInFlow onPrintRequest={setPassengerToPrint} />
+        </main>
+        <AirportFooter />
+      </div>
       {passengerToPrint && (
-        <div className="printable-area hidden">
+        <div className="printable-area">
           <BoardingPassPrint passenger={passengerToPrint} />
         </div>
       )}
-    </div>
+    </>
   );
 }
